15e2aa18ee57201e1b473949f2e50cf5dc226519
aqilah permata dinanti <aqilahpermatad@gmail.com>
2026-03-15 10:05:56 +0800

    pesan commit

--- Aqilah Program.py
+++ Aqilah Program.py
@@ -1,9 +1,16 @@
+
 #contoh penerapan library python
 import requests 
 
 response = requests.get('https://api.github.com')
 print(response.status_code)
 print(response.text)
+
+#library math 
+import math
+
+print(math.sqrt(20))
+print(math.pi)
 
 
 
